f220161cb89b8950d83c3a8ed30bc2c7d0c04033
lukem <@>
2025-10-14 10:13:58 -0700

    Added the last two views, those being Settings and Transactions.
    Transactions is just Add/Edit Finances.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 import Home from './Views/Home'
 import History from './Views/History'
+import Transactions from './Views/Transactions'
+import Settings from './Views/Settings'
 
 
 function App() {
@@ -21,6 +23,8 @@ function App() {
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/settings" element={<Settings />} />
     </Routes>
     </BrowserRouter>
   )
--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom'
 export default function Home() {
   return (
     <div>
-      <h1>Da finance tracker Home Page</h1>
+      <h1>Finance Tracker Home Page</h1>
       <Link to="/history">History</Link>
+      <br />
+      <Link to="/transactions">Transactions</Link>
+      <br />
+      <Link to="/settings">Settings</Link>
     </div>
   )
 }
